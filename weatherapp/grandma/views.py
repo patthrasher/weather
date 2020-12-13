c37cd8d2b61f2c4463ea831ad2ba34c4bb907bdf
patthrasher <patrickalanthrasher@gmail.com>
2020-12-13 16:33:43 -0600

    added environment variable

--- weatherapp/grandma/views.py
+++ weatherapp/grandma/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+import os
 import urllib.request, json
 from .forms import Weather_form
 from .models import Weather
 
+
 def index(request) :
 
+    key = os.environ.get('weather_key')
+    
     if request.method == 'POST' :
         form = Weather_form(request.POST or None)
 
@@ -14,21 +19,26 @@ def index(request) :
             city = str(Weather.objects.last())
             city = city.replace(' ', '%20')
 
-            service_url = 'http://api.weatherapi.com/v1/current.json?key=' + key + '&q=' + city
-            print('service url:', service_url)
+            try :
+                service_url = 'http://api.weatherapi.com/v1/current.json?key=' + key + '&q=' + city
+                print('service url:', service_url)
 
 
-            response = urllib.request.urlopen(service_url)
-            read = response.read()
-            data = json.loads(read)
+                response = urllib.request.urlopen(service_url)
+                read = response.read()
+                data = json.loads(read)
 
-            f = data['current']['temp_f']
+                f = data['current']['temp_f']
 
-            context = {
+                context = {
                 'f' : f,
-            }
+                }
 
-            return render(request, 'grandma/index.html', context)
+                return render(request, 'grandma/index.html', context)
+
+            except :
+                messages.error(request, 'Invalid City')
+                return redirect('grandma:index')
 
 
     return render(request, 'grandma/index.html')
